01b337146970469254b0a0a5da656961827dd789
webmaster444 <jlmobile710@gmail.com>
2018-08-21 21:35:17 +0300

    240 > row removed

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var currentTableNumber = null;
 var labels = ["01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "00:00"];
 var legends;
 var td_width;
-var removeKeys = ['Total', 'Still in'];
+var removeKeys = ['Total', 'Still in','> 240'];
 
 function draw_chart(date, tableNumber) {
     $('td').removeClass('highlight');
